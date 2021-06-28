5c821f66cdf4f4b5a4b2d8d0193c16f0152d5ca3
Brandi Cameron <hello@brandicameron.com>
2021-06-28 13:54:59 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const jsConfetti = new JSConfetti();
+let timer = null;
+const partyBtn = document.getElementById("party-btn");
+
+function confettiParty() {
+  jsConfetti.addConfetti({
+    confettiColors: [
+      "#ff0a54",
+      "#ff477e",
+      "#ff7096",
+      "#ff85a1",
+      "#fbb1bd",
+      "#f9bec7",
+    ],
+    confettiRadius: 10,
+    confettiesNumber: 50,
+    // emojies: ["Brandi"],
+    // emojiSize: 30,
+    // confettiesNumber: 10,
+  });
+}
+// runs entire time button is pressed
+function startParty() {
+  //this allows the confetti to work even if the person just quickly clicks the button instead of holding it down.
+  confettiParty();
+  timer = setInterval(() => {
+    confettiParty();
+  }, 300);
+}
+
+function endParty() {
+  clearInterval(timer);
+}
+
+partyBtn.addEventListener("mousedown", startParty);
+partyBtn.addEventListener("mouseup", endParty);
+partyBtn.addEventListener("touchstart", startParty);
+partyBtn.addEventListener("touchend", endParty);
